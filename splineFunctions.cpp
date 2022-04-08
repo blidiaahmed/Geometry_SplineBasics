cc395f4cc264e0f643aba1b4e9d219b9290ece1e
blidiaahmed <42154508+blidiaahmed@users.noreply.github.com>
2022-04-08 18:14:22 +0200

    Revert "code for generation of tensor with random dimension"

--- splineFunctions.cpp
+++ splineFunctions.cpp
@@ -2,8 +2,8 @@
 #include "splineFunctions.h"
 #include "tensor.h"
 //spline::spline(tensor Tensr) :BS(Tensr.get_dimension()),Tensor(Tensr) {}
-template <int i>
-unsigned __int8 spline<i>::get_dimension()
+
+unsigned __int8 spline::get_dimension()
 {
 
 	return Tensor.get_dimension();
--- tensor.cpp
+++ tensor.cpp
@@ -1,42 +1,23 @@
 #include"pch.h"
 #include"tensor.h"
-
-
-
-//template <>
-//tensor<vector<float>>::tensor(unsigned __int8 dim) {
-//	vect = vector<vector<float>>();
-//}
-//template <unsigned __int8 dim>
-//tensor<dim>::tensor()//:dimension(dim)
-//{
-//	//if (dim == 1)
-//	//{
-//	//	vect=vector<float>();
-//	//}
-//	//else
-//	//{
-//	//	//vect= vector<tensor>;
-//	//}
-//}
-
-//template <>
-//tensor<1>::tensor():dimension(1)
-//{
-//}
-
-//template <unsigned __int8 i>
-//tensor<i>::tensor(const tensor& T) : dimension(T.dimension), vect(T.vect) {}
-//template <unsigned __int8 i>
-//tensor<i>::tensor(tensor&& T) : dimension(T.dimension), vect(T.vect) {}
-//template <unsigned __int8 i>
-//tensor<i>::tensor(unsigned __int8 dim, vector<float> v) : dimension(dim), vect(v){};
-//template <unsigned __int8 i>
-//float tensor<i>::get_value(unsigned __int8 iIndex){return vect[iIndex];}
-//template <unsigned __int8 i>
-//unsigned __int8 tensor<i>::get_dimension(){return dimension;}
-//template <unsigned __int8 i>
-//vector<float> tensor<i>::get_vector(){	return vect;}
+//template <int i>
+tensor/*<i>*/::tensor(unsigned __int8 dim):dimension(dim)
+{
+	if (dim == 1)
+	{
+		vect=vector<float>();
+	}
+	else
+	{
+		//vect= vector<tensor>;
+	}
+}
+tensor::tensor(const tensor& T) : dimension(T.dimension), vect(T.vect) {}
+tensor::tensor(tensor&& T) : dimension(T.dimension), vect(T.vect) {}
+tensor::tensor(unsigned __int8 dim, vector<float> v) : dimension(dim), vect(v){};
+float tensor::get_value(unsigned __int8 iIndex){return vect[iIndex];}
+unsigned __int8 tensor::get_dimension(){return dimension;}
+vector<float> tensor::get_vector(){	return vect;}
 
 
 
